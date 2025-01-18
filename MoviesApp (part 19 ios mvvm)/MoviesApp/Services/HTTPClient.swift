825af66cf94b663b9c00da2e0ec4db91570ee565
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-01-18 14:07:12 +0700

    22. Implementing HTTP Client

---
+++ MoviesApp (part 19 ios mvvm)/MoviesApp/Services/HTTPClient.swift
@@ -0,0 +1,39 @@
+//
+//  HTTPClient.swift
+//  MoviesApp
+//
+//  Created by Rhony on 18/01/25.
+//  Copyright © 2025 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL
+    case noData
+    case decodingError
+}
+
+class HTTPClient {
+
+    func getMoviesBY(
+        search: String,
+        completion: @escaping (Result<[Movie]?, NetworkError>) -> Void
+    ) {
+        guard let url = URL(string: "http://www.omdbapi.com/?apikey=\(APIKeys.API_KEYS)&s=\(search)")
+            else {
+            return completion(.failure(.badURL))
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                return completion(.failure(.noData))
+            }
+            guard let movieResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else {
+                return completion(.failure(.decodingError))
+            }
+            completion(.success(movieResponse.movies))
+        }.resume()
+    }
+
+}
--- MoviesApp (part 19 ios mvvm)/MoviesApp/Utils/Constants.swift
+++ MoviesApp (part 19 ios mvvm)/MoviesApp/Utils/Constants.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 struct Constants {
-    static let API_KEY = "YOURAPIKEY" // OMDBAPI
+    
 }
