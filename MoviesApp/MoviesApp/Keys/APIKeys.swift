21a4a9eecfcfc5af173357751d030be57d633df7
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-01-28 09:46:59 +0700

    Rename folder

---
+++ MoviesApp/MoviesApp/Keys/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  MoviesApp
+//
+//  Created by Rhony on 18/01/25.
+//  Copyright © 2025 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let API_KEYS = "5748f8a1"
+}
---
+++ MoviesApp/MoviesApp/Screens/MovieListScreen.swift
@@ -0,0 +1,26 @@
+//
+//  MovieListScreen.swift
+//  MoviesApp
+//
+//  Created by Rhony on 27/01/25.
+//  Copyright © 2025 Mohammad Azam. All rights reserved.
+//
+
+import SwiftUI
+
+struct MovieListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListScreen()
+}
+
+// second way to provide preview
+struct MovieListScreen_Preview: PreviewProvider {
+    static var previews: some View {
+        MovieListScreen()
+    }
+}
