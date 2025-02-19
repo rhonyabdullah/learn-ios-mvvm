2ad8a1eae31514cecd765eaa7e2134826def9170
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-02-19 09:25:19 +0700

    66. Presenting Sheets for WeatherListScreen and AddCityScreen

--- WeatherAppSwiftUI/WeatherAppSwiftUI/Screens/WeatherListScreen.swift
+++ WeatherAppSwiftUI/WeatherAppSwiftUI/Screens/WeatherListScreen.swift
@@ -7,31 +7,55 @@
 
 import SwiftUI
 
+enum Sheets: Identifiable {
+    var id: UUID {
+        return UUID()
+    }
+    case addNewCity
+    case settings
+}
 
 struct WeatherListScreen: View {
-    
-        
+
+    @State private var activeSheet: Sheets?
+
     var body: some View {
-        
+
         List {
             ForEach(1...20, id: \.self) { index in
                 Text("\(index)")
             }
-            }
+        }
         .listStyle(PlainListStyle())
-        
-        .navigationBarItems(leading: Button(action: {
-           
-        }) {
-            Image(systemName: "gearshape")
-        }, trailing: Button(action: {
-            
-        }, label: {
-            Image(systemName: "plus")
-        }))
+        .sheet(
+            item: $activeSheet,
+            content: { (item) in
+                switch item {
+                case .addNewCity:
+                    AddCityScreen()
+                case .settings:
+                    SettingsScreen()
+                }
+            }
+        )
+
+        .navigationBarItems(
+            leading: Button(action: {
+                activeSheet = .settings
+            }) {
+                Image(systemName: "gearshape")
+            },
+            trailing: Button(
+                action: {
+                    activeSheet = .addNewCity
+                },
+                label: {
+                    Image(systemName: "plus")
+                })
+        )
         .navigationTitle("Cities")
         .embedInNavigationView()
-       
+
     }
 }
 
@@ -42,7 +66,7 @@ struct WeatherListScreen_Previews: PreviewProvider {
 }
 
 struct WeatherCell: View {
-    
+
     var body: some View {
         HStack {
             VStack(alignment: .leading, spacing: 15) {
@@ -58,13 +82,21 @@ struct WeatherCell: View {
                 }
             }
             Spacer()
-            
-            
+
             Text("72 F")
         }
         .padding()
-        .background(Color(#colorLiteral(red: 0.9133135676, green: 0.9335765243, blue: 0.98070997, alpha: 1)))
-        .clipShape(RoundedRectangle(cornerRadius: 10, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+        .background(
+            Color(
+                #colorLiteral(
+                    red: 0.9133135676, green: 0.9335765243, blue: 0.98070997,
+                    alpha: 1))
+        )
+        .clipShape(
+            RoundedRectangle(
+                cornerRadius: 10,
+                style: /*@START_MENU_TOKEN@*/ .continuous /*@END_MENU_TOKEN@*/
+            ))
 
     }
 }
